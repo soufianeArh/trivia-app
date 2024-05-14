46a3bd0052e3c4a7ecbb608583c5abf1f7bb5c97
Jannis Seemann <5215310+jannis-seemann@users.noreply.github.com>
2024-05-14 14:31:34 +0300

    .

---
+++ server.ts
@@ -0,0 +1,37 @@
+import process from "process"
+import cors from "cors"
+import express from "express"
+import mongoose from "mongoose"
+import compression from "compression"
+import ensureInitialData from "./import"
+import apiRouter from "./api"
+
+const app = express();
+app.use(compression());
+app.use(express.json());
+app.use(cors({
+    origin: [
+        "http://localhost:5173"
+    ]
+}))
+app.use(express.urlencoded());
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+const mongoDbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/trivia';
+console.log(mongoDbUri);
+const db = mongoose.connect(mongoDbUri, {
+    connectTimeoutMS: 1000
+});
+db.catch(() => {
+    console.log(`Failed to connect to mongodb: ${mongoDbUri}`)
+    process.exit(1)
+})
+
+ensureInitialData();
+
+app.use("/api", apiRouter);
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
